97b3638f5ee520e7b0fb8533c7949c2651bfc4e2
Amy Kim <amesk3@gmail.com>
2019-08-18 11:51:31 -0400

    bars working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # d3RevenueGraph
+
+A simple bar graph that visualizes revenue over a period of seven months. Coursework from a udemy course called, "Mastering data visualization in D3.js."
+
+##To begin
+python3 -m http.server
--- js/main.js
+++ js/main.js
@@ -4,25 +4,57 @@
  *    Project 1 - Star Break Coffee
  */
 
+var width = 600;
+var height = 400;
+
 var svg = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", "400")
-  .attr("height", "400");
+  .attr("width", width)
+  .attr("height", height);
 
 d3.json("data/revenues.json").then(function(data) {
+  console.log(data);
+
   data.forEach(d => {
     d.revenue = +d.revenue;
   });
 
-  var y = d3
-    .scaleLiner()
-    .domain([0, 800])
-    .range([0, 400]);
+  var x = d3
+    .scaleBand()
+    .domain(
+      data.map(function(d) {
+        return d.month;
+      })
+    )
+    .range([0, width])
+    .paddingInner(0.3)
+    .paddingOuter(0.3);
 
-  var rects = svg
-    .selectAll("rect")
-    .data(data)
+  var y = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(data, function(d) {
+        return d.revenue;
+      })
+    ])
+    .range([0, height]);
+
+  var rects = svg.selectAll("rect").data(data);
+
+  rects
     .enter()
-    .append("rect");
+    .append("rect")
+    .attr("y", function(d) {
+      return y(d.revenue);
+    })
+    .attr("x", function(d) {
+      return x(d.month);
+    })
+    .attr("width", x.bandwidth)
+    .attr("height", function(d) {
+      return height - y(d.revenue);
+    })
+    .attr("fill", "grey");
 });
